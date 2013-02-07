2a1947bbfacefa2a2cbac2d4e58d65222150f18e
Daniel Melzer <dm@digital-control.biz>
2013-02-07 17:30:04 +0100

    Initial commit.

---
+++ Json.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Teacup;
+
+
+class Json {
+
+
+	/**
+	 * @var array
+	 */
+	static protected $errors = array(
+			JSON_ERROR_CTRL_CHAR => 'Control character error',
+			JSON_ERROR_DEPTH => 'Maximum stack depth has been exceeded',
+			JSON_ERROR_NONE => 'No error has occured',
+			JSON_ERROR_STATE_MISMATCH => 'Malformed or invalid JSON',
+			JSON_ERROR_SYNTAX => 'Syntax error',
+			JSON_ERROR_UTF8 => 'Malformed UTF-8 characters'
+	);
+
+
+	const HEX_TAG = JSON_HEX_TAG;
+	const HEX_AMP = JSON_HEX_AMP;
+	const HEX_APOS = JSON_HEX_APOS;
+	const HEX_QUOT = JSON_HEX_QUOT;
+	const FORCE_OBJECT = JSON_FORCE_OBJECT;
+	const NUMERIC_CHECK = JSON_NUMERIC_CHECK;
+
+
+	/**
+	 * Encodes a value to a JSON string and checks for errors.
+	 *
+	 * @static
+	 * @param mixed $value
+	 * @param int $options
+	 * @return string
+	 * @throws \RuntimeException
+	 */
+	static public function encode($value, $options = 0) {
+		$return = @json_encode($value, $options);
+		$lastError = json_last_error();
+
+		if(JSON_ERROR_NONE !== $lastError) {
+			throw new \RuntimeException(static::$errors[$lastError]);
+		}
+
+		return $return;
+	}
+
+	/**
+	 * Decodes a json string and checks for errors.
+	 *
+	 * @static
+	 * @param string $json
+	 * @param bool $assoc
+	 * @param int $depth
+	 * @return mixed
+	 * @throws \RuntimeException
+	 */
+	static public function decode($json, $assoc = false, $depth = 512) {
+		$return = @json_decode($json, $assoc, $depth);
+		$lastError = json_last_error();
+
+		if(JSON_ERROR_NONE !== $lastError) {
+			throw new \RuntimeException(static::$errors[$lastError]);
+		}
+
+		return $return;
+	}
+
+}
---
+++ tests/Json.php
@@ -0,0 +1,104 @@
+<?php
+
+define('DOCUMENT_ROOT', __DIR__ . '/../');
+require DOCUMENT_ROOT . 'Json.php';
+
+
+class Json extends \PHPUnit_Framework_TestCase {
+
+
+	/**
+	 * @var array
+	 */
+	private $array = array('<foo>',"'bar'",'"baz"','&blong&');
+
+
+	public function testEncode() {
+		$result = Teacup\Json::encode($this->array);
+		$this->assertJsonStringEqualsJsonString($result, '["<foo>","\'bar\'","\"baz\"","&blong&"]');
+	}
+
+	public function testEncodeOptionTag() {
+		$result = Teacup\Json::encode($this->array, Teacup\Json::HEX_TAG);
+		$this->assertJsonStringEqualsJsonString(
+				$result, '["\u003Cfoo\u003E","\'bar\'","\"baz\"","&blong&"]');
+	}
+
+	public function testEncodeOptionApos() {
+		$result = Teacup\Json::encode($this->array, Teacup\Json::HEX_APOS);
+		$this->assertJsonStringEqualsJsonString(
+				$result, '["<foo>","\u0027bar\u0027","\"baz\"","&blong&"]');
+	}
+
+	public function testEncodeOptionQuot() {
+		$result = Teacup\Json::encode($this->array, Teacup\Json::HEX_QUOT);
+		$this->assertJsonStringEqualsJsonString(
+				$result, '["<foo>","\'bar\'","\u0022baz\u0022","&blong&"]');
+	}
+
+	public function testEncodeOptionAmp() {
+		$result = Teacup\Json::encode($this->array, Teacup\Json::HEX_AMP);
+		$this->assertJsonStringEqualsJsonString(
+				$result, '["<foo>","\'bar\'","\"baz\"","\u0026blong\u0026"]');
+	}
+
+	public function testEncodeOptionAll() {
+		$result = Teacup\Json::encode(
+				$this->array,
+				Teacup\Json::HEX_TAG|Teacup\Json::HEX_APOS|
+						Teacup\Json::HEX_QUOT|Teacup\Json::HEX_AMP
+		);
+		$this->assertJsonStringEqualsJsonString(
+				$result,
+				'["\u003Cfoo\u003E","\u0027bar\u0027","\u0022baz\u0022","\u0026blong\u0026"]'
+		);
+	}
+
+	public function testEncodeOptionObject() {
+		$result = Teacup\Json::encode($this->array, Teacup\Json::FORCE_OBJECT);
+		$this->assertJsonStringEqualsJsonString(
+				$result,
+				'{"0":"<foo>","1":"\'bar\'","2":"\"baz\"","3":"&blong&"}'
+		);
+	}
+
+	public function testEncodeOptionNumber() {
+		$result = Teacup\Json::encode(array('test' => '1337'), Teacup\Json::NUMERIC_CHECK);
+		$this->assertEquals($result, '{"test":1337}');
+	}
+
+	/**
+	 * @expectedException RuntimeException
+	 * @expectedExceptionMessage Malformed UTF-8 characters
+	 */
+	public function testEncodeFailingUtf8() {
+		Teacup\Json::encode(array(utf8_decode('äüß')));
+	}
+
+	public function testDecode() {
+		$result = Teacup\Json::decode('{"a":1,"b":2,"c":3,"d":4,"e":5}');
+		$this->assertInstanceof('stdClass', $result);
+	}
+
+	public function testDecodeArray() {
+		$result = Teacup\Json::decode('["<foo>","\'bar\'","\"baz\"","&blong&"]', true);
+		$this->assertEquals($result, $this->array);
+	}
+
+	/**
+	 * @expectedException RuntimeException
+	 * @expectedExceptionMessage Maximum stack depth has been exceeded
+	 */
+	public function testDecodeFailingDepth() {
+		Teacup\Json::decode('["<foo>","\'bar\'","\"baz\"","&blong&"]', false, 1);
+	}
+
+	/**
+	 * @expectedException RuntimeException
+	 * @expectedExceptionMessage Syntax error
+	 */
+	public function testDecodeFailingSyntax() {
+		Teacup\Json::decode("{'test': 'failing away'}");
+	}
+
+}
